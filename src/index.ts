7c1034fe55ecc6017a1fd30b27407a972995eca0
Benoît Hubert <benoithubert@gmail.com>
2022-04-27 20:06:50 +0200

    Setup Apollo Server, TypeGraphQL and TypeGraphQL Prisma

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server';
+import { PrismaClient } from '@prisma/client';
+import { resolvers } from './generated/graphql';
+import { buildSchema } from 'type-graphql';
+
+const prisma = new PrismaClient();
+
+// A `main` function so that you can use async/await
+async function main() {
+  const schema = await buildSchema({
+    resolvers,
+    validate: false,
+  });
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({ schema, context: { prisma } });
+
+  // The `listen` method launches a web server.
+  const { url } = await server.listen();
+  console.log(`🚀  Server ready at ${url}`);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
